ae5cee63992c7d862ab5dfc35e172e6ba08a4b9d
Dan <bdylan998@gmail.com>
2019-04-15 09:27:50 +0300

    Initial Commit

---
+++ Segue_passing_data_4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Segue_passing_data_4
+//
+//  Created by dainis.putans on 15/04/2019.
+//  Copyright © 2019 dainis.putans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
